c0d3516a4181e1e78642ca0efe51ebbb455533e0
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2022-06-08 22:41:43 -0300

    update README.md

---
+++ src/main/java/com/fredson/umlusecase/UmlUseCaseApplication.java
@@ -0,0 +1,12 @@
+package com.fredson.umlusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlUseCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmlUseCaseApplication.class, args);
+    }
+}
